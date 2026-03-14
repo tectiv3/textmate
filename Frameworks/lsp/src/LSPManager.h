8b702a65a7d2bc2dbbe6cb8c99ce4d944b2cc47f
tectiv3 <git@getpanda.me>
2026-03-14 02:45:40 +0000

    lsp: shutdown all LSP servers on app termination

---
+++ Frameworks/lsp/src/LSPManager.h
@@ -0,0 +1,15 @@
+#ifndef LSP_MANAGER_H_POC
+#define LSP_MANAGER_H_POC
+
+#import <document/OakDocument.h>
+
+@interface LSPManager : NSObject
++ (instancetype)sharedManager;
+- (void)documentDidOpen:(OakDocument*)document;
+- (void)documentDidChange:(OakDocument*)document;
+- (void)documentDidSave:(OakDocument*)document;
+- (void)documentWillClose:(OakDocument*)document;
+- (void)shutdownAll;
+@end
+
+#endif /* LSP_MANAGER_H_POC */
